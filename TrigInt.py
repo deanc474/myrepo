7cceefe5b4740904f27e8feac523937a381af943
deanc474 <cddean@ualberta.ca>
2017-06-05 14:38:06 -0600

    Add files via upload

--- TrigInt.py
+++ TrigInt.py
@@ -3,6 +3,8 @@ import matplotlib.pyplot as plt
 
 def TrigInt(x,N):
     
+    #This function evaluates the interpolation points
+    
     if N % 2 == 1: #odd
         tau = np.sin(N*np.pi*x/2)/(N*np.sin(np.pi * x/2))
     else: #even
--- TrigInterp.py
+++ TrigInterp.py
@@ -1,6 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
 def TrigInterp(x,y,Nnew):
     #Trigonometric Interpolation
     #Input
@@ -10,22 +14,41 @@ def TrigInterp(x,y,Nnew):
     #Output
     #P = New vector of appropriate length Nnew
     
+    #Note This function does not output an x vector.
+    
     #Define out interpolation points
     N=len(x)
-    xi = np.linspace(x[0],x[N-1],Nnew)
-    h=2/N
-    scale = (x[2]-x[1])/h
+    X = np.linspace(x[0],x[N-1],Nnew)
+    y = np.asmatrix(y) #Necessary for matrix multiplication
+    
+    xi = np.linspace(x[0],x[N-1],Nnew)#New x data points
+    
+    h=2/N 
+    scale = (x[2]-x[1])/h 
     x = x/scale
+    x=np.asmatrix(x) #Necessary for matrix multiplication
     xi = xi/scale
+    xi = np.asmatrix(xi) #Necessary for matrix multiplication
     
     #evaluate the interpolation points
-    P = np.zeros(Nnew)
+    Y = np.zeros(Nnew)
+    Y = np.asmatrix(Y)
     
-    for k in range(N):
-        P = P + y[k]*TrigInt(xi-x[k],N)
     
-    return P
-            
+    #To avoid using a for loop we instead use matrix multiplication
+    
+    h=np.ones(N)
+    h=np.asmatrix(h)
+    k=np.ones(Nnew)
+    k=np.asmatrix(k)
+    
+    Y = Y + np.dot(y,TrigInt(np.transpose(np.dot(np.transpose(xi),h))-np.dot(np.transpose(x),k),N))
+    # [1xNnew] = [1xNnew] + [1xN] * (([1xNnew]'*[1xN])' - ([1xN]'*[1xNnew]))
+    # [1xNnew] = [1xNnew] + [1xN] * [NxNnew]
+    Y = np.transpose(Y)
+
+    
+    return X,Y
     
     
 
